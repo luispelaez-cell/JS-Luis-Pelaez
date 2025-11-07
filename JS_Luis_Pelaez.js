7d6c7e8ffa19da1b6ecc630a7c8614c6440f857b
luisp <luis.pelaez@institutvidreres.cat>
2025-11-07 10:51:15 +0100

    Ex3

--- JS_Luis_Pelaez.js
+++ JS_Luis_Pelaez.js
@@ -1,6 +1,7 @@
 console.log("Luis")
 console.log("Pelaez Martos")
 console.log("2007")
-var jamoniqueso= true
+let jamoniqueso= true
 let bocatalomo= "ya"
 const sixorseven = 67
+console.log(jamoniqueso)
