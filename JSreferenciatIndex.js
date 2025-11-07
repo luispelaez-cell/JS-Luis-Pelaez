9b9b8421600551c97ab9278ee8cb5151e64d1ebc
luisp <luis.pelaez@institutvidreres.cat>
2025-11-07 11:35:00 +0100

    ex7

--- JSreferenciatIndex.js
+++ JSreferenciatIndex.js
@@ -5,3 +5,8 @@ let rest = number - bigint
 let div = number/ bigint
 let mult = number * bigint
 console.log(sum.toFixed(), div.toFixed() , mult.toFixed())
+
+let catet1 = 4;
+let catet2 = 4;
+let hipotenusa = Math.hypot(catet1, catet2)
+console.log(hipotenusa.toFixed(2))
