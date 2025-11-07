09ce10ca2e1415da61c94891a29f2830b3347e07
luisp <luis.pelaez@institutvidreres.cat>
2025-11-07 11:30:43 +0100

    ex6

---
+++ JSreferenciatIndex.js
@@ -0,0 +1,7 @@
+let number= 1234
+let bigint = 5.678
+let sum = number + bigint
+let rest = number - bigint
+let div = number/ bigint
+let mult = number * bigint
+console.log(sum.toFixed(), div.toFixed() , mult.toFixed())
